6af932c4b1bb199172c7671b92e1954c5eb93dd4
DaamiAle <daamiale.recoil@gmail.com>
2024-02-07 22:42:43 -0300

    Entity models

---
+++ src/Domain/IAM.Domain.Entity/Model/ProfileEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IAM.Domain.Entity.Model;
+
+public class ProfileEntity
+{
+    public int Id { get; set; }
+    public string XMLProfile { get; set; }
+    [Required]
+    public int UserId { get; set; }
+    [ForeignKey("UserId")]
+    public UserEntity User { get; set; }
+    public bool RegState { get; set; }
+}
